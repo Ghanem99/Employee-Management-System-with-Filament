c27055799ff75ee8c02281aad884165a4b8b0d7b
Ghanem99 <moghanem345@gmail.com>
2024-04-27 05:34:44 +0300

    Set the Relations between the models!

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Country extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'country_code',
+        'name'
+    ];
+
+    public function employees(): HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
+    
+    public function states(): HasMany
+    {
+        return $this->hasMany(State::class);
+    }
+
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class);
+    }
+
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,10 +2,44 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'city_id',
+        'state_id',
+        'country_id',
+        'department_id',
+        'address',
+        'zip_code',
+        'birth_date',
+        'date_hired', 
+    ];
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
 }
