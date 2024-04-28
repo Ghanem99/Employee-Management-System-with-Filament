b921ff075b568d2a034d53f110f0b376ddedf8b1
Ghanem99 <moghanem345@gmail.com>
2024-04-28 18:59:31 +0300

    Build Employee Resource CRUD with Filter feature!

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -2,16 +2,22 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\EmployeeResource\Pages;
-use App\Filament\Resources\EmployeeResource\RelationManagers;
-use App\Models\Employee;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\Employee;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\EmployeeResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\EmployeeResource\RelationManagers;
 
 class EmployeeResource extends Resource
 {
@@ -23,7 +29,51 @@ class EmployeeResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Card::make([
+                    TextInput::make('first_name')
+                        ->label('First Name')
+                        ->required()
+                        ->placeholder('John'),
+                    TextInput::make('last_name')
+                        ->label('Last Name')
+                        ->required()
+                        ->placeholder('Doe'),
+                    TextInput::make('address')
+                        ->label('Address')
+                        ->required()
+                        ->placeholder('1234 Elm St.'),
+                    TextInput::make('zip_code')
+                        ->label('Zip Code')
+                        ->required()
+                        ->placeholder('12345'),
+                    DatePicker::make('birth_date')
+                        ->label('Birth Date')
+                        ->required(),
+                    DatePicker::make('date_hired')
+                        ->label('Date Hired')
+                        ->required(),
+                    Select::make('department_id')
+                        ->label('Department')
+                        ->placeholder('Select the department')
+                        ->relationship('department', 'name')
+                        ->required(),
+                    Select::make('country_id')
+                        ->label('Country')
+                        ->placeholder('Select the country')
+                        ->relationship('country', 'name')
+                        ->required(),
+                    Select::make('state_id')
+                        ->label('State')
+                        ->placeholder('Select the state')
+                        ->relationship('state', 'name')
+                        ->required(),
+                    Select::make('city_id')
+                        ->label('City')
+                        ->placeholder('Select the city')
+                        ->relationship('city', 'name')
+                        ->required(),
+                    
+                ])
             ]);
     }
 
@@ -31,10 +81,51 @@ class EmployeeResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('first_name')
+                    ->label('First Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('last_name')
+                    ->label('Last Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('address')
+                    ->label('Address')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('zip_code')
+                    ->label('Zip Code')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('birth_date')
+                    ->label('Birth Date')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('date_hired')
+                    ->label('Date Hired')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('department.name')
+                    ->label('Department')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('country.name')
+                    ->label('Country')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('state.name')
+                    ->label('State')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('city.name')
+                    ->label('City')
+                    ->searchable()
+                    ->sortable(),
             ])
             ->filters([
-                //
+                SelectFilter::make('department_id')
+                    ->label('Department')
+                    ->relationship('department', 'name'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
